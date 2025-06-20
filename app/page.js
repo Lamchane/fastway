cb102a8ca23469dd421c162ea1ef74e0b5ca3898
lamchane <sagargurung9004@gmail.com>
2025-06-20 17:32:08 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,103 +1,223 @@
+import React from "react";
 import Image from "next/image";
-
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
+import { IoCallOutline } from "react-icons/io5";
+import { MdOutlineEmail } from "react-icons/md";
+import Link from "next/link";
+import Blurtexture from "./components/blurtexture";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <section className="overflow-hidden">
+      {/* Navbar */}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+      <nav className="border-blue-600 bg-gradient-to-r from-blue-600 via-blue-900 to-blue-400">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            <img
+              src="/assets/images/logo.png"
+              className="h-10"
+              alt="fastway_logo"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            {/* <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Fastway
+            </span> */}
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </nav>
+
+      {/* end navbar */}
+      <section>
+        {/* <Blurtexture />
+         */}
+        {/* <Blurtexture className={"rotate-180"} /> */}
+        <div className="p-2 md:py-12 mx-auto max-w-7xl">
+          <h2 className="text-5xl font-semibold text-primary text-center">
+            Become Our <span className="text-red-700">Customers</span>
+          </h2>
+          <p className="py-2 text-center text-gray-600 max-w-2xl mx-auto">
+            We offer dependable, secure, and convenient services through our
+            unique pick&ndash;up and delivery model. Our efficient delivery
+            system guarantees same&ndash;day delivery right to your doorstep.
+          </p>
+
+          <div className="p-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="w-full h-[300px] md:h-[600px] overflow-hidden rounded-xl">
+              <Image
+                src="/assets/images/c1.jpg"
+                width={640}
+                height={960}
+                alt="Contact illustration"
+                className="w-full h-full object-cover rounded-xl"
+              />
+            </div>
+            <div>
+              <form
+                action="https://formsubmit.co/info@fastwaycourier.in"
+                method="POST"
+                className="space-y-8 py-4"
+              >
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    placeholder="name@gmail.com"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="phone"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900"
+                  >
+                    Phone number
+                  </label>
+                  <input
+                    type="number"
+                    id="phone"
+                    name="phone"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    placeholder="phone number"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="subject"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900"
+                  >
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    id="subject"
+                    name="subject"
+                    className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-600 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    placeholder="Let us know how we can help you"
+                    required
+                  />
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="message"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900"
+                  >
+                    Your message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows="6"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-600 dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Leave a message..."
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-red-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Send message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="p-4 bg-gradient-to-r from-blue-600 via-blue-900 to-blue-400 text-white">
+        <div className="p-4 pt-12 flex flex-col md:flex-row justify-between gap-8 flex-wrap">
+          {/* Left: About + Socials */}
+          <div>
+            <div>
+              <img
+                src="/assets/images/logo.png"
+                className="h-14 w-40"
+                alt="fastway_logo"
+              />
+            </div>
+            <p className="py-2 text-sm">
+              Our services ensure prompt, safe, and <br />
+              seamless deliveries tailored to your needs.
+            </p>
+            <div className="py-4 flex space-x-2">
+              <div className="p-2 h-8 w-8 bg-rose-600 flex justify-center items-center">
+                <Link
+                  target="_blank"
+                  href="https://www.instagram.com/fastwaycouriersofficial/?igsh=Y3JmNTVmanNoazNk#"
+                >
+                  <FaInstagram size={20} color="white" />
+                </Link>
+              </div>
+              <div className="p-2 h-8 w-8 bg-blue-700 flex justify-center items-center">
+                <Link
+                  target="_blank"
+                  href="https://www.facebook.com/fastwaycouriersofficial/"
+                >
+                  <FaFacebookF size={20} color="white" />
+                </Link>
+              </div>
+              <div className="p-2 h-8 w-8 bg-red-700 flex justify-center items-center">
+                <FaYoutube size={20} color="white" />
+              </div>
+            </div>
+          </div>
+
+          {/* Right: Contact Info */}
+          <div className="space-y-4">
+            <h2 className="text-2xl font-semibold">Contact Us</h2>
+
+            <div className="p-2 border border-accent w-full max-w-sm">
+              <div className="flex space-x-2">
+                <div className="p-2 h-8 w-8 bg-red-700 flex justify-center items-center rounded-sm">
+                  <IoCallOutline size={20} color="white" />
+                </div>
+                <div>
+                  <h2 className="font-semibold">Call Us</h2>
+                  <Link href="tel:+916026802908" className="text-sm">
+                    +91 6026802908
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="p-2 border border-accent w-full max-w-sm">
+              <div className="flex space-x-2">
+                <div className="p-2 h-8 w-8 bg-red-700 flex justify-center items-center rounded-sm">
+                  <MdOutlineEmail size={20} color="white" />
+                </div>
+                <div>
+                  <h2 className="font-semibold">Email Us</h2>
+                  <Link
+                    href="mailto:info@fastwaycourier.in"
+                    className="text-sm"
+                  >
+                    info@fastwaycourier.in
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Bottom Line */}
+        <div className="py-4 text-center text-sm">
+          © All Copyright & Reserved by &ndash;
+          <Link target="_blank" href="https://linktr.ee/GoVisible">
+            Govisible
+          </Link>
+        </div>
       </footer>
-    </div>
+    </section>
   );
 }
